3d5475416369b60170b695a5f1b83a87b8dedf21
Garrett Schmahl <gschmahl@pm.me>
2021-04-05 13:07:22 -0700

    insertion sort

--- sort.py
+++ sort.py
@@ -8,9 +8,10 @@ def bubbleSort(sortDict):
       if sortDict[i] > sortDict[i+1]:
         sortDict[i], sortDict[i+1] = sortDict[i+1], sortDict[i]
         confirmSort = True
+  return True
 #print(myNewDict)     
 #bubbleSort(myNewDict)
-print(myNewDict)
+#print(myNewDict)
 
 def selectionSort(sortDict):
   for i in range(len(sortDict)):
@@ -20,5 +21,21 @@ def selectionSort(sortDict):
         lowestRemainingValIndex = j
     if lowestRemainingValIndex != i:
       sortDict[i], sortDict[lowestRemainingValIndex] = sortDict[lowestRemainingValIndex], sortDict[i]
+  return True
+#selectionSort(myNewDict)
+#print(myNewDict)
+
+def insertionSort(sortDict):
+  for i in range(1,len(sortDict)): #iteration loop where i is our current index
+    if sortDict[i] < sortDict[i-1]: #check if it needs to run for this value
+      swapVal = sortDict[i]
+      for j in range(i, 1, -1): #for loop with if statement that works backwards until it finds an index containing a smaller value
+        if swapVal > sortDict[j-1]: 
+          sortDict[j] = swapVal
+          break
+        else:
+          sortDict[j] = sortDict[j-1]
+  return True
 selectionSort(myNewDict)
 print(myNewDict)
+  
