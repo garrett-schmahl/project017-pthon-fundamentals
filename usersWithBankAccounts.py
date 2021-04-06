eaf9f00c6ad50438195bdbea9fb31849df6861b9
Garrett Schmahl <gschmahl@pm.me>
2021-04-06 15:05:12 -0700

    adding print messages

--- usersWithBankAccounts.py
+++ usersWithBankAccounts.py
@@ -14,16 +14,22 @@ class User:
 
   def deposit(self, amount, accountType):
     self.accounts[accountType].balance += amount
+    print(f"Deposited {amount} into {self.name}'s {accountType} account. Current balance is ${self.accounts[accountType].balance:.2f}")
     return self
 
   def withdraw(self, amount, accountType):
     self.accounts[accountType].balance -= amount
+    print(f"Withdrew {amount} from {self.name}'s {accountType} account. Current balance is ${self.accounts[accountType].balance:.2f}")
     return self
 
-  def yieldInterest(self, accountType):
+  def yieldInterest(self):
     for account in self.accounts:
       if self.accounts[account].balance > 0:
-        self.accounts[account].balance = self.accounts[account].balance+self.accounts[account].balance*self.accounts[account].interestRate
+        print(f"Balance of your {account} account is {self.accounts[account].balance:.2f}. Your account's interest rate is {self.accounts[account].interestRate}")
+        self.accounts[account].balance += self.accounts[account].balance*self.accounts[account].interestRate
+        print(f"The new balance of your {account} account is {self.accounts[account].balance:.2f}")
+      else:
+        print("Error: You are poor ;.;")
     return self
 
   def transferMoney(self, selfAccountType, target, targetAccountType, amount):
@@ -36,7 +42,7 @@ class User:
     print(f"Hello {self.name}")
     print(f"Your email is is {self.email}")
     for account in self.accounts.keys():
-      print(f"Your {account} balance is {self.accounts[account].balance}")
+      print(f"Your {account} balance is {self.accounts[account].balance:.2f}")
       print(f"Your {account} interest rate is {self.accounts[account].interestRate}")
     return self
   
@@ -45,19 +51,19 @@ class User:
     return self
 
 
-kaguya = User("Kaguya", "kaguya@shinomiya.love")
-chika = User("Chika", "chika@fujiwara.dance")
-miyuki = User("Miyuki", "miyuki@shirogane.pres")
+kaguya = User("Kaguya Shinomiya", "kaguya@shinomiya.love")
+chika = User("Chika Fujiwara", "chika@fujiwara.dance")
+miyuki = User("Miyuki Shirogane", "miyuki@shirogane.pres")
 
 
 #user 1 tasks
-kaguya.deposit(10, "checking").deposit(5, "saving").deposit(15, "checking").withdraw(15, "checking").displayAccountInfo()
+kaguya.deposit(10, "checking").deposit(5, "saving").deposit(15, "checking").withdraw(15, "checking").yieldInterest().displayAccountInfo()
 
 #user 2 tasks
-chika.deposit(5, "saving").deposit(20, "checking").withdraw(5, "saving").withdraw(10, "checking").displayAccountInfo()
+chika.deposit(5, "saving").deposit(20, "checking").withdraw(5, "saving").withdraw(10, "checking").yieldInterest().displayAccountInfo()
 
 #user 3 tasks
-miyuki.deposit(100, "checking").withdraw(10, "checking").withdraw(10, "checking").withdraw(10, "checking").displayAccountInfo()
+miyuki.deposit(100, "checking").withdraw(10, "checking").withdraw(10, "checking").withdraw(10, "checking").yieldInterest().displayAccountInfo()
 
 #challenge
 kaguya.transferMoney("saving", miyuki, "checking", 10).displayAccountInfo()
