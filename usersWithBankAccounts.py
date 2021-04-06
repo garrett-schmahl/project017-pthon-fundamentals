ab36c0dc8b5f9c466b943c95d64ae3a1d2e56ab1
Garrett Schmahl <gschmahl@pm.me>
2021-04-06 14:16:24 -0700

    update for better account control

--- usersWithBankAccounts.py
+++ usersWithBankAccounts.py
@@ -1,61 +1,66 @@
-class bankAccount:
-  def __init__(self, balance, interestRate, accountType):
-    self.accountType = accountType
+class BankAccount:
+  def __init__(self, balance = 0, interestRate = .01):
     self.balance = balance
     self.interestRate = interestRate
 
-class user:
+class User:
   def __init__(self, name, email):
     self.name = name
     self.email = email
-    self.accountSaving = bankAccount(balance=0, interestRate=0.02, accountType ="savings")
-    self.accountChecking = bankAccount(balance=0, interestRate=0.01, accountType ="checking")
+    self.accounts = {
+      "checking": BankAccount(),
+      "saving": BankAccount(0, .02)
+    }
+
   def deposit(self, amount, accountType):
-    if accountType == "checking":
-      self.accountChecking.balance += amount
-    elif accountType == "saving":
-      self.accountSaving.balance += amount
+    self.accounts[accountType].balance += amount
     return self
+
   def withdraw(self, amount, accountType):
-    if accountType == "checking":
-      self.accountChecking.balance -= amount
-    elif accountType == "saving":
-      self.accountSaving.balance -= amount
+    self.accounts[accountType].balance -= amount
     return self
+
+  def yieldInterest(self, accountType):
+    for account in self.accounts:
+      if self.accounts[account].balance > 0:
+        self.accounts[account].balance = self.accounts[account].balance+self.accounts[account].balance*self.accounts[account].interestRate
+    return self
+
+  def transferMoney(self, selfAccountType, target, targetAccountType, amount):
+    self.withdraw(amount, selfAccountType)
+    target.deposit(amount, targetAccountType)
+    print(f"Transferred {amount} from {self.name}'s {selfAccountType} account, to {target.name}'s {targetAccountType} account.")
+    return self
+
   def displayAccountInfo(self):
     print(f"Hello {self.name}")
     print(f"Your email is is {self.email}")
-    print(f"Your checking balance is {self.accountChecking.balance}")
-    print(f"Your checking interest rate is {self.accountChecking.interestRate}")
-    print(f"Your savings balance is {self.accountSaving.balance}")
-    print(f"Your savings interest rate is {self.accountSaving.interestRate}")
+    for account in self.accounts.keys():
+      print(f"Your {account} balance is {self.accounts[account].balance}")
+      print(f"Your {account} interest rate is {self.accounts[account].interestRate}")
     return self
-  def yieldInterest(self):
-    if self.accountChecking.balance > 0:
-      self.accountChecking.balance = self.accountChecking.balance+self.accountChecking.balance*self.accountChecking.interestRate
-    if self.accountSaving.balance > 0:
-      self.accountSaving.balance = self.accountSaving.balance+self.accountSaving.balance*self.accountSaving.interestRate
-    return self
-  def transferMoney(self, other, amount):
-    self.accountChecking.balance -= amount
-    other.accountChecking.balance += amount
+  
+  def createNewAccount(self, newAccountName, newAccountBalance, newAccountInterestRate):
+    self.accounts[newAccountName] = BankAccount(newAccountBalance, newAccountInterestRate)
     return self
 
 
+kaguya = User("Kaguya", "kaguya@shinomiya.love")
+chika = User("Chika", "chika@fujiwara.dance")
+miyuki = User("Miyuki", "miyuki@shirogane.pres")
 
-kaguya = user("Kaguya", "kaguya@shinomiya.love")
-chika = user("Chika", "chika@fujiwara.dance")
-miyuki = user("Miyuki", "miyuki@shirogane.pres")
 
-
-print(kaguya.name)
+#user 1 tasks
 kaguya.deposit(10, "checking").deposit(5, "saving").deposit(15, "checking").withdraw(15, "checking").displayAccountInfo()
 
-print(chika.name)
+#user 2 tasks
 chika.deposit(5, "saving").deposit(20, "checking").withdraw(5, "saving").withdraw(10, "checking").displayAccountInfo()
 
-print(miyuki.name)
+#user 3 tasks
 miyuki.deposit(100, "checking").withdraw(10, "checking").withdraw(10, "checking").withdraw(10, "checking").displayAccountInfo()
 
-kaguya.transferMoney(miyuki, 10).displayAccountInfo()
+#challenge
+kaguya.transferMoney("saving", miyuki, "checking", 10).displayAccountInfo()
 miyuki.displayAccountInfo()
+
+kaguya.createNewAccount("secret_account", 500, .05).displayAccountInfo()
